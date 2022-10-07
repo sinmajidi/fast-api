0df78625224ce20cd66589dff629cf973bd0e1ab
sina majidi <sin.majidi@gmail.com>
2022-10-07 16:38:08 +0330

    i wnat to add status code to project

--- 01_send a parameter to function.py
+++ 01_send a parameter to function.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,status
 import uvicorn
 app=FastAPI()
 @app.get('/')
@@ -7,10 +7,14 @@ def index():
 @app.get('/blogs')
 def get_blogs(page_size,count):
 	return {'message':f'{page_size} ---- {count}'}
-@app.get('/blog/{id}')
-def get_blog(id):
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK)
+def get_blog(id,response:Response):
 	if id.isnumeric():
-		return {'message':'blogs!','id':f'{id}'}
+		if int(id)<10:
+			return {'message':'blogs!','id':f'{id}'}
+		else:
+			response.status_code=status.HTTP_404_NOT_FOUND
+			return {'message': f'blog {id} not founde'}
 	else:
 		return {'message': 'your entrance should be integer'}
 
