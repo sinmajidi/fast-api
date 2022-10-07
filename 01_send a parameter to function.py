08785a2b016d749839352b79f05adc724a8155b2
sina majidi <sin.majidi@gmail.com>
2022-10-07 20:28:32 +0330

    now i add descriptions,tags and summary to api's

--- 01_send a parameter to function.py
+++ 01_send a parameter to function.py
@@ -4,10 +4,23 @@ app=FastAPI()
 @app.get('/')
 def index():
 	return 'homes'
-@app.get('/blogs')
+
+# you can add description using this command also and you can see tags,summary and description in /docs
+# @app.get('/blogs',tags=['blog'],summary='get all blogs!', description = 'this api is for get all blogs'
+# ,response_description='now you can see all the blogs')
+@app.get('/blogs',tags=['blog'],summary='get all blogs!',response_description='now you can see all the blogs')
 def get_blogs(page_size,count):
+	
+	"""
+	this api is for get all blogs
+	- **page_size:** for getting page size
+	- **count:** for getting blogs count
+	"""
 	return {'message':f'{page_size} ---- {count}'}
-@app.get('/blog/{id}',status_code=status.HTTP_200_OK)
+
+
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK,tags=['blog'],summary='get a blog!',description='this api is for '
+                                                                                                     'getting blogs')
 def get_blog(id,response:Response):
 	if id.isnumeric():
 		if int(id)<10:
