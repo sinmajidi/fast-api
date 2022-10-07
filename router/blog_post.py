816619a84037a38a5482d3c37ee77f3a49f72754
sina majidi <sin.majidi@gmail.com>
2022-10-08 02:12:09 +0330

    making a post base api for example a api for addig post in a weblog

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
+from typing import Optional
 router=APIRouter(prefix='/blog',tags=['blog'])
+
+class BlogModel(BaseModel):
+	title:str
+	content:str
+	nb_comments:int
+	publisher:Optional[bool]
+
+
 @router.post('/new')
-def creat_post():
-	return {'massage':'ok'}
+def creat_post(blog:BlogModel):
+	return {'massage':'ok','data':blog}
